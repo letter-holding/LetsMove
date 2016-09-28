69f8c3187f60dc85fafbc8a8388516deb555fb4b
Jeff McLeman <jmcleman@blackpixel.com>
2016-09-28 15:54:41 -0700

    Case 60707 - update LetsMove to latest that uses AppleScript as a last resort to deleted app after move.

--- PFMoveApplication.h
+++ PFMoveApplication.h
@@ -1,12 +1,10 @@
 //
-//  PFMoveApplication.h, version 1.7.2
+//  PFMoveApplication.h, version 1.22
 //  LetsMove
 //
 //  Created by Andy Kim at Potion Factory LLC on 9/17/09
 //
 //  The contents of this file are dedicated to the public domain.
-
-#import <Foundation/Foundation.h>
 
 #ifdef __cplusplus
 extern "C" {
